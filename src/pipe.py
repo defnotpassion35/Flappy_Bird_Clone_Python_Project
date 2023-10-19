3ce85eeed6c68d0ac4f40303577c07683ff5e60e
quan-le <31929073+quan-le@users.noreply.github.com>
2023-10-19 08:13:48 +0700

    Test switching branch

--- src/pipe.py
+++ src/pipe.py
@@ -17,12 +17,3 @@ class Pipe:
         # Implement the pipe's movement
         pass
     
-<<<<<<< HEAD
-    #methangquangbeo
-    #me thang quan ebo 
-    #mdfasdfjlsdjf
-    #dhfalsdfs
-    
-=======
-    #test name naem ane m
->>>>>>> de6fa97161e985dcbee2e5a86fbac3a7b06d7c79
