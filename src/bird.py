8d25b646bcea4d148136824e02e702e7e2ea9356
Đình Quang Nguyễn <nguyendinhquang7272004@gmail.com>
2023-12-03 11:06:39 +0700

    12332423432asdas

--- src/bird.py
+++ src/bird.py
@@ -1,42 +1,39 @@
-# Create a Bird class
 import pygame
 import os
-#import main
 
 class FlappyBird(pygame.sprite.Sprite):
-    #still have to add image and sound into init
-    def __init__(self, x, y): #, img
-        # Initialize attributes for the bird
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        
-        #Bird (Initial) lOcation
-        self.x = x 
-        self.y = y 
-        
-        #Bird movement attribute
-        self.Y_velocity = -200
-        self.gravity = 1
-        #self.sound[collide]
-        
-        #adding image
+
+        # Bird location
+        self.x = x
+        self.y = y
+
+        # Bird movement attributes
+        self.Y_velocity = -125
+        self.gravity = 5
+
+        # Bird image and rect
         cwd = os.path.dirname(__file__)
-        self.img = pygame.image.load(os.path.join(cwd, "img", "plane.png"))
-        self.rect = self.img.get_rect()
-        self.rect.center = (x,y)
-        
+        self.image = pygame.image.load(os.path.join(cwd, "img", "plane.png"))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
     def flap(self):
         # Implement the bird's jump action
         print("the bird is flapping")
         self.rect.y += self.Y_velocity
-        
 
-    def update(self,screen_height):
+    def update(self, screen_height):
         # Implement the bird's movement (gravity)
         self.rect.y += self.gravity
-        print("current y position: seft.rect.y: " + str(self.rect.y))
-        # also limit to not allow bitd move out of the screen (vertically)
+        #print("current y position: self.rect.y: " + str(self.rect.y))
+
+        # Limit to not allow bird to move out of the screen (vertically)
         if self.rect.top < 0:
             self.rect.top = 0
-            #we could possible kill the bird or just not allow them to go pass the border
         if self.rect.bottom > screen_height:
             self.rect.bottom = screen_height
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
--- src/pipe.py
+++ src/pipe.py
@@ -1,46 +1,31 @@
-#code bird class, include many attributes and methods
-#--------------------Attributes or properties----------------------------------
-#possible function attributes: x_position, top and bottom pipe,
-# possible instace attribute: heigth, width 
+import pygame
+import random
 
-#-----------------Methods-----------------------
-#Possible methods: draw(), update()
-# Create a Pipe class
-class Pipe:
-    #define constant 
-    PIPE_WIDTH = 80
-    PIPE_GAP = 200
-    PIPE_X = WIDTH
-    PIPE_SPEED = 2
+screen_width = 1920
+screen_height = 1080
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, height, gap):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.height = height
+        self.gap = gap
+        self.width = 80
+        self.rect = pygame.Rect(self.x, 0, self.width, self.height)
+        self.lower_rect = pygame.Rect(self.x, int(self.height + self.gap), self.width, int(screen_height - self.height - self.gap))
+
+    def update(self, speed):
+        # Move the pipe to the left
+        self.x -= speed
+        self.rect.x = self.x
+
+    def offscreen(self):
+        # Check if the pipe is offscreen
+        return self.x + self.width < 0
+
+    def draw(self, screen, pipe_color):
+    # Draw the upper part of the pipe
+        pygame.draw.rect(screen, pipe_color, (self.x, 0, self.width, int(self.height)))
     
-    def __init__(self,x,y,):
-        # Initialize attributes for the pipe
-        pass
-
-    def move(self):
-        # Implement the pipe's movement
-        pass
-    def draw(self):
-        pass
-    
-#Testing genvironment
-#intialize game 
-pygame.init()
-   
-   #define the resolution
-screen = pygame.display.set_mode((1280,720))
-   
-   #Title
-pygame.display.set_caption("Flappy Bird Clone")
-   #Icon
-   
-   #Game Loop
-gameOn = True
-   
-while gameOn:
-    for event in pygame.event.get():
-        if event.type() == pygame.QUIT:
-            gameOn = False
-      #update display
-    pygame.display.update()
-
+    # Draw the lower part of the pipe
+        pygame.draw.rect(screen, pipe_color, (self.x, int(self.height + self.gap), self.width, int(screen_height - self.height - self.gap)))
