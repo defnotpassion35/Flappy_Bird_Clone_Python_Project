0e7425dd20b02678e76c5f486291b854a3c892d2
quan-le <31929073+quan-le@users.noreply.github.com>
2024-01-02 13:19:42 +0700

    aoply inheritance and polymorphism concept for bird.py

--- src/bird.py
+++ src/bird.py
@@ -10,7 +10,7 @@ class FlappyBird(pygame.sprite.Sprite):
     img_string = None
         
     #CONSTRUCOTRS
-    def __init__(self, x, y, imagePath): #, img_string
+    def __init__(self, x, y, imagePath): 
         # Initialize attributes for the bird
         pygame.sprite.Sprite.__init__(self)
         
@@ -24,11 +24,9 @@ class FlappyBird(pygame.sprite.Sprite):
         self.Y_velocity = -110
         self.gravity = 4
         
-        #self.sound[collide]
-        
         #adding image
         cwd = os.path.dirname(__file__)
-        self.image = pygame.image.load(os.path.join(cwd, "img", imagePath))
+        self.image = pygame.image.load(os.path.join(cwd, "img", "Bird_1.png")) #, imagePath))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -54,3 +52,40 @@ class FlappyBird(pygame.sprite.Sprite):
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+        
+class Bird1(FlappyBird):
+    def __init__(self, x, y, imagePath):
+        super().__init__(x, y, imagePath)
+        #adding image
+        cwd = os.path.dirname(__file__)
+        self.image = pygame.image.load(os.path.join(cwd, "img", "Bird_1.png")) #, imagePath))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    def flap(self):
+        print("the bird is flapping 1 time faster")
+        self.rect.y += 1 * self.Y_velocity
+        
+        
+class Bird2(FlappyBird):
+    def __init__(self, x, y, imagePath):
+        super().__init__(x, y, imagePath)
+        #adding image
+        cwd = os.path.dirname(__file__)
+        self.image = pygame.image.load(os.path.join(cwd, "img", "Bird_2.png")) #, imagePath))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    def flap(self):
+        print("the bird is flapping 1.5 time faster")
+        self.rect.y += 1.5 * self.Y_velocity
+        
+class Bird3(FlappyBird):
+    def __init__(self, x, y, imagePath):
+        super().__init__(x, y, imagePath)
+        #adding image
+        cwd = os.path.dirname(__file__)
+        self.image = pygame.image.load(os.path.join(cwd, "img", "Bird_3.png")) #, imagePath))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    def flap(self):
+        print("the bird is flapping 2 time faster")
+        self.rect.y += 2 * self.Y_velocity
--- src/game_test.py
+++ src/game_test.py
@@ -2,6 +2,9 @@ import pygame
 import sys
 import random
 from bird import FlappyBird 
+from bird import Bird1
+from bird import Bird2
+from bird import Bird3
 from pipe import Pipe
 import math
 from button import Button
@@ -17,7 +20,6 @@ class Game:
         pygame.display.set_caption("Flappy Bird")
 
         # Set up colors 
-
         self.GRAY = (128, 128, 128)
 
         # Set up clock
@@ -28,7 +30,7 @@ class Game:
         self.pipes = pygame.sprite.Group()
 
         # Set up the bird
-        self.bird = FlappyBird(screen_width // 4, screen_height // 2, "plane.png")  # Initial position
+        self.bird = Bird2(screen_width // 4, screen_height // 2, "plane.png")  # Initial position
         self.birds.add(self.bird)
 
         # Set up pipes
